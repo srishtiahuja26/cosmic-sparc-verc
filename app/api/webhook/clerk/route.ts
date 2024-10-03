b0d9b0076d6dc34ea5da765efd623ee29b65bbd0
AESiR <cyborgkiller1008@gmail.com>
2024-10-03 13:38:05 +0530

    Refactor Clerk webhook route to handle empty last name and image URL

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -65,8 +65,8 @@ export async function POST(req: Request) {
       email: email_addresses[0].email_address,
       username: username!,
       firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
+      lastName: last_name || ' ',
+      photo: image_url || ' ',
     };
     console.log(user);
     
