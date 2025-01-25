2d8b44bcbcdffa09da4451ec4417bceadc6aa492
Redskull-127 <meerraja17@gmail.com>
2025-01-25 21:31:20 +0530

    bun

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['utfs.io'],
 		remotePatterns: [
 			{
-				protocol: 'https',
 				hostname: 'utfs.io',
-				port: '',
 			},
 		],
 	},
